12b12a3e5ce90e627284296d913684c3e4a82824
root <root@LAPTOP-8J6BCMEC.localdomain>
2018-08-20 14:26:10 -0400

    Added bootstrap dependency and personal information for newton Massachusetts

--- src/App.js
+++ src/App.js
@@ -20,19 +20,38 @@ class Results extends Component {
             countryCode: "US",
         };
     }
+    convertTemp(input) {
+        return 9/5 * (input - 273) + 32;
+    };
+    createView() {
+        return (
+            <div class="jumbotron">
+                <h1 class="display-4"> Welcome to Weather App! </h1>
+                <p class="lead"> This is a simple weather app personalized for Adam Qian </p>
+                <hr class="my-4"/>
+                <p> It is completely built off of React and is my pet project for experimenting with react </p>
+                <p> Current weather in Newton Massachusetts:</p>
+                <h4> <b> Temperature: {this.convertTemp(this.state.main.temp)}F</b></h4>
+                <h4> <b> Temperature range: {this.convertTemp(this.state.main.temp_min)}F - {this.convertTemp(this.state.main.temp_max)}F</b></h4>
+                <h4> <b> Humidity: {this.state.main.humidity}%</b></h4>
+                <h4> <b> Wind: Speed: {this.state.wind.speed * 0.621371} MPH  Direction: {this.state.wind.deg} degrees</b> </h4>
+                <h4> <b> Current Weather: {this.state.weather.description} </b></h4>
+            </div>
+        );
+    }
     componentDidMount() {
-        const apiURL = `api.openweathermap.org/data/2.5/weather?zip=${this.state.zipcode},${this.state.countryCode}&APPID=${this.props.APPID}`;
+        const apiURL = `http://api.openweathermap.org/data/2.5/weather?zip=${this.state.zipcode},${this.state.countryCode}&APPID=${this.props.APPID}`;
         console.log(apiURL);
         fetch(apiURL)
         .then(results => {
-            // if (!results.ok) throw Error(results.statusText);
+            if (!results.ok) throw Error(results.statusText);
             console.log(results);
             return results.json();
         }).then(data=>{
             this.setState({
                 coord: data.coord,
                 sys: data.sys,
-                weather: data.weather,
+                weather: data.weather[0],
                 base: data.base,
                 main: data.main,
                 wind: data.wind,
@@ -47,7 +66,7 @@ class Results extends Component {
     return(
         <div className="Results">
             <p> This is the results section </p>
-            {this.state.main.temp}
+            {this.createView()}
         </div>
     );
   }
